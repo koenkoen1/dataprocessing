0615cf9e327818d53051b65d136a73a4408814a5
koenkoen1 <43954486+koenkoen1@users.noreply.github.com>
2018-11-08 14:05:49 +0100

    fixed unintentional deletion of rows due to missing unrelevant values

--- homework/Week_2/eda.py
+++ homework/Week_2/eda.py
@@ -14,12 +14,16 @@ data_dict = {key: [] for key in stats}
 with open('input.csv', 'r') as csvfile:
     reader = csv.DictReader(csvfile)
     for row in reader:
-        if 'unknown' in row.values() or '' in row.values():
+        if ('unknown' in (row[stat] for stat in stats) or
+            '' in (row[stat] for stat in stats)):
             continue
         for stat in stats:
             try:
-                data_dict[stat].append(float(row[stat].rstrip(' dollars')))
+                data_dict[stat].append(float(row[stat]))
             except:
-                data_dict[stat].append(row[stat].rstrip())
+                if 'dollars' in row[stat]:
+                    data_dict[stat].append(int(row[stat].rstrip(' dollars')))
+                else:
+                    data_dict[stat].append(row[stat].rstrip(' '))
     dataframe = pandas.DataFrame(data_dict)
-    print(dataframe)
+    print(dataframe.to_string())
