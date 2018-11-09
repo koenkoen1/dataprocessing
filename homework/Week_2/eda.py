0969c70d285361395cd1e1c0bf832c2041a7fddf
koenkoen1 <43954486+koenkoen1@users.noreply.github.com>
2018-11-09 12:38:00 +0100

    making histograms since 2018

--- homework/Week_2/eda.py
+++ homework/Week_2/eda.py
@@ -4,26 +4,43 @@
 
 import csv
 import pandas
+import numpy as np
+import matplotlib.pyplot as plt
 
-stats = ['Country', 'Region', 'Pop. Density (per sq. mi.)',
+heads = ['Country', 'Region', 'Pop. Density (per sq. mi.)',
          'Infant mortality (per 1000 births)', 'GDP ($ per capita) dollars']
 
-data_dict = {key: [] for key in stats}
+data_dict = {key: [] for key in heads}
+
+def parse():
+    with open('input.csv', 'r') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            if ('unknown' in (row[stat] for stat in heads) or
+                '' in (row[stat] for stat in heads)):
+                continue
+            for stat in heads:
+                try:
+                    data_dict[stat].append(float(row[stat]))
+                except:
+                    if 'dollars' in row[stat]:
+                        data_dict[stat].append(int(row[stat].rstrip(' dolars')))
+                    else:
+                        data_dict[stat].append(row[stat].rstrip())
+
+def central_tendency(df):
+    print(f"mean: {df.mean()}")
+    print(f"median: {df.median()}")
+    print(f"mode: {df.mode()}")
+    print(f"std: {df.std()}")
+    values = df.values
+    plt.hist(values)
+    plt.show()
 
 
-with open('input.csv', 'r') as csvfile:
-    reader = csv.DictReader(csvfile)
-    for row in reader:
-        if ('unknown' in (row[stat] for stat in stats) or
-            '' in (row[stat] for stat in stats)):
-            continue
-        for stat in stats:
-            try:
-                data_dict[stat].append(float(row[stat]))
-            except:
-                if 'dollars' in row[stat]:
-                    data_dict[stat].append(int(row[stat].rstrip(' dollars')))
-                else:
-                    data_dict[stat].append(row[stat].rstrip(' '))
-    dataframe = pandas.DataFrame(data_dict)
-    print(dataframe.to_string())
+
+if __name__ == "__main__":
+    parse()
+    df = pandas.DataFrame(data_dict)
+    print(df.to_string())
+    central_tendency(df.loc[:, 'GDP ($ per capita) dollars'])
