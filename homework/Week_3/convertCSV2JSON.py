28d0ababeb0decbd8656aaad5e6b826e377b7ea1
koenkoen1 <43954486+koenkoen1@users.noreply.github.com>
2018-11-19 10:55:29 +0100

    comments

--- homework/Week_3/convertCSV2JSON.py
+++ homework/Week_3/convertCSV2JSON.py
@@ -9,11 +9,15 @@ data_dict = {}
 
 def csv_to_json(filename):
     """
-    parses csv file and converts to json format
+    parses csv file and converts to json
     """
     with open(f"{filename}.csv", 'r') as csvfile:
         reader = csv.DictReader(csvfile)
+
+        # the first field is assumed to be a unique identifier
         field = reader.fieldnames[0]
+
+        # parse csvfile
         for row in reader:
             tempdict = {}
             for key in row:
@@ -21,6 +25,7 @@ def csv_to_json(filename):
                     tempdict[key] = row[key]
             data_dict[row[field]] = tempdict
 
+    # create jsonfile
     with open(f"{filename}.json", 'w') as outfile:
         json.dump(data_dict, outfile, indent=4)
 
