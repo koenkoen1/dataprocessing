7dc231c3c064fb0488c9eb1a557c5f0e87b079f9
koenkoen1 <43954486+koenkoen1@users.noreply.github.com>
2018-11-27 21:01:30 +0100

    graph without axes

---
+++ homework/Week_4/convertCSV2JSON.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# Name: Koen Dielhoff
+# Student number: 11269693
+
+import csv
+import json
+
+def csv_to_json(filename):
+    """
+    parses csv file and converts to json
+    """
+    data_dict = {}
+    with open(f"{filename}.csv", 'r') as csvfile:
+        reader = csv.DictReader(csvfile)
+
+        # the first field is assumed to be a unique identifier
+        field = reader.fieldnames[0]
+
+        # parse csvfile to json format
+        for row in reader:
+            tempdict = {}
+            for key in row:
+                if not key == field:
+                    tempdict[key] = row[key]
+            data_dict[row[field]] = tempdict
+
+    # create jsonfile
+    with open(f"{filename}.json", 'w') as outfile:
+        json.dump(data_dict, outfile, indent=4)
+
+if __name__ == "__main__":
+    csv_to_json("data")
