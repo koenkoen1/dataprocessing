43cd2a108d374e3b36427376602705e70cc4e147
koenkoen1 <43954486+koenkoen1@users.noreply.github.com>
2018-12-17 13:46:11 +0100

    linked, commented, made things nicer

--- homework/Week_6/script.js
+++ homework/Week_6/script.js
@@ -5,16 +5,27 @@ interactive views
 */
 window.onload = function() {
   d3.json("prunedData.json").then(function(data) {
+
+    // parse data and create bargraph
     let parsedData = dataParse(data);
-    console.log(parsedData);
     createBarGraph(parsedData);
+
+    // reparse as previous parse does not fit
     let reparsedData = calenderParse(parsedData);
-    console.log(reparsedData["North America"]);
+
+    // create initial calender graph with data from North America
     createCalender(reparsedData["North America"]);
+
+    // update calender graph when bars in bargraph is clicked
+    d3.selectAll(".bar").on("click", function(d) {
+      d3.select(".calenderGraph").select(".graphTitle").text("Terrorism in " + d.x);
+      calenderUpdate(reparsedData[d.x]);
+    });
   })
 }
 
 function dataParse(data) {
+  // saves dates of datapoints
   let dict = {};
   let keys = Object.keys(data)
   keys.forEach(function(key) {
@@ -29,7 +40,7 @@ function dataParse(data) {
 }
 
 function createBarGraph(data) {
-  // height, width and margin
+  // height, width, barpadding and margin
   const w = 1000;
   const h = 500;
   const barPadding = 5;
@@ -42,6 +53,13 @@ function createBarGraph(data) {
             .attr("width", w)
             .attr("height", h);
 
+  // adds graphtitle to svg
+  svg.append("text")
+     .attr("transform", "translate(" + (w/2) + ", 12)")
+     .style("text-anchor", "middle")
+     .text("Global terrorism");
+
+  // calculates the amount of datapoints per country
   let countries = Object.keys(data);
   let newData = [];
   countries.forEach(function(country) {
@@ -49,12 +67,10 @@ function createBarGraph(data) {
     newData.push(countryData);
   })
 
-  console.log(newData);
-
   // creates scales for the data
   let yScale = d3.scaleLinear()
             .domain([0, Math.max.apply(Math, newData.map(function(o) { return o.y; }))])
-            .range([h - margin, margin])
+            .range([h - 1.5 * margin, margin])
             .nice();
   let xScale = d3.scaleBand()
             .domain(countries)
@@ -63,17 +79,20 @@ function createBarGraph(data) {
   let yAxis = d3.axisLeft(yScale);
   let xAxis = d3.axisBottom(xScale);
 
+  // adds bargraphbars
   svg.selectAll("rect")
       .data(newData)
       .enter()
       .append("rect")
-      .attr("class", "bar")
-      .attr("x", function(d) { return xScale(d.x); })
-      .attr("y", function(d){ return yScale(d.y); })
-      .attr("width", xScale.bandwidth)
-      .attr("height", function(d){
-        return h - yScale(d.y) - margin;
-      })
+        .attr("class", "bar")
+        .attr("x", function(d) { return xScale(d.x); })
+        .attr("y", function(d) { return yScale(d.y); })
+        .attr("width", xScale.bandwidth)
+        .attr("height", function(d){
+          return h - yScale(d.y) - 1.5 * margin
+        })
+        .append("title")
+          .text(function(d) {return "Amount: " + d.y; });
 
   // adds axes
   svg.append("g")
@@ -82,24 +101,27 @@ function createBarGraph(data) {
      .call(yAxis);
   svg.append("g")
      .attr("class", "axis")
-     .attr("transform", "translate(0," + (h - margin) + ")")
-     .call(xAxis);
+     .attr("transform", "translate(0," + (h - 1.5 * margin) + ")")
+     .call(xAxis)
+    .selectAll("text")
+      .attr("transform", "rotate(6)")
 
    // adds axes labels
    svg.append("text")
       .attr("transform", "translate(" + (w/2) + "," + (h-5) + ")")
       .style("text-anchor", "middle")
-      .text("countries");
+      .text("Region");
    svg.append("text")
       .attr("transform", "rotate(-90)")
       .attr("y", 0)
       .attr("x",0 - (h/2))
       .attr("dy", "1em")
       .style("text-anchor", "middle")
-      .text("amount of terrorist strikes");
+      .text("Amount of terrorist attacks");
 }
 
 function calenderParse(data) {
+  // combines datapoints with the same date
   countries = Object.keys(data);
   parsedData = {}
   countries.forEach(function(country) {
@@ -125,16 +147,17 @@ function calenderParse(data) {
 }
 
 function createCalender(data) {
+  // calender based on https://beta.observablehq.com/@mbostock/d3-calendar-view
+
+  // width, size of calender cells and height
   const w = 964;
   const cellSize = 17;
   const h = cellSize * 9;
 
+  // nests data per year
   const years = d3.nest()
       .key(d => d[0].getFullYear())
     .entries(data)
-    .reverse();
-
-  console.log(years)
 
   // initializes a svg
   let svg = d3.select("body")
@@ -143,14 +166,24 @@ function createCalender(data) {
             .attr("width", w)
             .attr("height", (h * years.length));
 
+  // adds graphtitle to svg
+  svg.append("text")
+     .attr("transform", "translate(" + (w/2) + ", 12)")
+     .attr("class", "graphTitle")
+     .style("text-anchor", "middle")
+     .text("Terrorism in North America");
+
+  // initializes a group for every year
   let year = svg.selectAll("g")
           .data(years)
           .enter()
           .append("g")
+          .attr("class", function(d) { return "year" + d.key; })
           .attr("transform", function(d, i) {
             return "translate(40, " + (h * i + 1.5 * cellSize) + ")";
           });
 
+  // adds year title
   year.append("text")
       .attr("x", -5)
       .attr("y", -5)
@@ -158,6 +191,7 @@ function createCalender(data) {
       .attr("text-anchor", "end")
       .text(function(d) { return d.key; });
 
+  // adds legend for days
   year.append("g")
       .attr("text-anchor", "end")
     .selectAll("text")
@@ -167,15 +201,50 @@ function createCalender(data) {
       .attr("y", d => (d.getDay() + 0.8) * cellSize)
       .text(d => "SMTWTFS"[d.getDay()]);
 
+  // adds cells to calender
   year.append("g")
+    .attr("class", "rects")
     .selectAll("rect")
     .data(function(d) { return d.values})
     .enter().append("rect")
+      .attr("class", "calenderBlock")
       .attr("width", cellSize - 1)
       .attr("height", cellSize - 1)
       .attr("x", d => d3.timeMonday.count(d3.timeYear(d[0]), d[0]) * cellSize + 0.5)
       .attr("y", d => d[0].getDay() * cellSize)
       .attr("fill", "darkred")
     .append("title")
-      .text(function(d) { return "" + d[0] + d[1]});
+      .text(function(d) { return "Terrorist attacks: " + d[1] + " Date: " + d[0].toLocaleDateString(); })
+}
+
+function calenderUpdate(data) {
+  const cellSize = 17
+
+  // removes previous cells from calender
+  d3.selectAll("rect.calenderBlock").remove()
+
+  // nests data per year
+  const years = d3.nest()
+      .key(d => d[0].getFullYear())
+    .entries(data)
+
+  // iterates over every year group
+  for (key in years) {
+    // replaces cells in calender
+    d3.selectAll("g.year" + years[key].key)
+        .selectAll("rect")
+        .data(years[key].values)
+        .enter().append("rect")
+          .attr("class", "calenderBlock")
+          .attr("width", cellSize - 1)
+          .attr("height", cellSize - 1)
+          .attr("x", d => d3.timeMonday.count(d3.timeYear(d[0]), d[0]) * cellSize + 0.5)
+          .attr("y", d => d[0].getDay() * cellSize)
+          .attr("fill", "darkred")
+        .append("title")
+          .text(function(d) { return "Terrorist attacks: " + d[1] + " Date: " + d[0].toLocaleDateString(); })
+
+  }
+
+
 }
